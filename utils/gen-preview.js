23cd067182256194b2ab7b8069a785df8b99a002
Layton Whiteley <layton.w@smart-host.com>
2020-11-04 16:26:11 +0100

    ci: fix dynamic preview generation for better testing

---
+++ utils/gen-preview.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const path = require('path');
+
+const writeFile = (filePath, html) => {
+  const fd = path.resolve(__dirname, '..', filePath, 'index.html');
+  const contentBuf = Buffer.from(html || 'test content', {
+    encoding: 'utf-8',
+  });
+
+  try {
+    fs.unlinkSync(fd);
+  } catch (e) {
+    console.error('Could not delete file => ', e.message);
+  }
+
+  try {
+    fs.writeFileSync(fd, contentBuf);
+    console.log('Wrote content to file: ', fd);
+  } catch (e) {
+    console.error('Could not write file => ', e.message);
+  }
+};
+
+const allowedVars = [
+  'GITHUB_ACTOR',
+  'GITHUB_RUN_NUMBER',
+  'GITHUB_ACTION',
+  'GITHUB_REF',
+  'GITHUB_SHA',
+];
+
+(() => {
+  const filePath = process.argv.slice(2)[0];
+  const valueMap = process.env;
+
+  const content = Object.entries(valueMap)
+    .filter(([x]) => allowedVars.includes(x))
+    .map(([key, val]) => {
+      return `<div><b>${key}:</b> ${val}</div>`;
+    });
+
+  if (!content.length) {
+    console.log(`no content received`);
+    return writeFile(filePath, 'test content');
+  }
+
+  console.log(`received: ${content}`);
+  const contentHtml = content.join('\n   ');
+  const html = `<div>
+   ${contentHtml}
+</div>`;
+
+  return writeFile(filePath, html);
+})();
