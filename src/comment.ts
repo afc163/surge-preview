84bf57d171261766588a894f188f42052c3f8bf6
afc163 <afc163@gmail.com>
2020-12-03 20:58:40 +0800

    fix ts

--- src/comment.ts
+++ src/comment.ts
@@ -15,7 +15,7 @@ export async function findPreviousComment(
     issue_number,
   });
   const h = headerComment(header);
-  return comments.find((comment) => comment.body.includes(h));
+  return comments.find((comment) => comment.body?.includes(h));
 }
 
 export async function updateComment(
