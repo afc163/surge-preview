1c2cd46847774f2695cf49f8db5d95e2b1488f1a
liufu.lf <liufu.lf@antfin.com>
2021-05-22 16:15:19 +0800

    fix: use npm

--- start.sh
+++ start.sh
@@ -20,31 +20,31 @@ cd ./${project_name}
 
 echo "\033[49;32m \n******* ${project_name} installing *******\n \033[0m"
 
-tnpm i
+npm i
 
 echo "\033[49;32m \n******* ${project_name} building *******\n \033[0m"
 
-tnpm run dist
+npm run dist
 
 cd ../surge-preview/web-server/client
 
 echo "\033[49;32m \n******* client installing *******\n \033[0m"
 
-tnpm i
+npm i
 
 echo "\033[49;32m \n******* client building *******\n \033[0m"
 
-tnpm run build
+npm run build
 
 cd ../server
 
 echo "\033[49;32m \n******* server installing *******\n \033[0m"
 
-tnpm i
+npm i
 
 echo "\033[49;32m \n******* server starting *******\n \033[0m"
 
-tnpm start
+npm start
 
 # cd ..
 
